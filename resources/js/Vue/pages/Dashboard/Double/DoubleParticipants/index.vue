7dc692853b2d0d70814b66d90fd29055b814c065
Кудайназаров Булат <hudainazarov.b@yandex.ru>
2021-06-23 09:53:28 +0300

    double-code-refactoring

--- resources/js/Vue/pages/Dashboard/Double/DoubleParticipants/index.vue
+++ resources/js/Vue/pages/Dashboard/Double/DoubleParticipants/index.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="double-part">
         <div class="double-part__col">
-            <ColumnHeader amount="23" players="71" type="blue" />
+            <ColumnHeader :amount="groupedBetSums.blue" :players="groupedBets.blue.length" type="blue" />
             <ParticipateButton type="blue" />
             <ul class="double-part__list">
                 <ParticipantItem :key="index" v-for="(bet, index) in groupedBets.blue" :bet="bet" />
             </ul>
         </div>
         <div class="double-part__col">
-            <ColumnHeader amount="251" players="31" type="gold" />
+            <ColumnHeader :amount="groupedBetSums.gold" :players="groupedBets.gold.length" type="gold" />
             <ParticipateButton type="gold" />
             <ul class="double-part__list">
                 <ParticipantItem :key="index"  v-for="(bet, index) in groupedBets.gold" :bet="bet" />
             </ul>
         </div>
         <div class="double-part__col">
-            <ColumnHeader amount="231" players="10" type="green" />
+            <ColumnHeader :amount="groupedBetSums.green" :players="groupedBets.green.length" type="green" />
             <ParticipateButton type="green" />
             <ul class="double-part__list">
                 <ParticipantItem :key="index"  v-for="(bet, index) in groupedBets.green" :bet="bet" />
@@ -41,6 +41,13 @@ export default {
                 green: this.bets.filter(bet => bet.color === "green"),
                 blue: this.bets.filter(bet => bet.color === "blue"),
                 gold: this.bets.filter(bet => bet.color === "gold"),
+            }
+        },
+        groupedBetSums() {
+            return {
+                green: this.groupedBets.green.reduce((sum, bet) => sum + bet.value, 0),
+                blue: this.groupedBets.blue.reduce((sum, bet) => sum + bet.value, 0),
+                gold: this.groupedBets.gold.reduce((sum, bet) => sum + bet.value, 0),
             }
         },
     },
